83e41c317d2648e24a5b88995515777fa2f7e1f0
Md Danish Alam <danishalam002@gmail.com>
2022-08-12 21:11:59 +0530

    Codes of functions and Varibales

---
+++ script.js
@@ -0,0 +1,37 @@
+//Function declaration
+function addition(){
+    var num1 = 6;
+    var num2 = 5;
+    var sum = num1+num2;
+    document.write("sum is"+" "+sum);
+}
+
+function subtract(){
+    var num1 = 6;
+    var num2 = 5;
+    var sub = num1-num2;
+    document.write("sub is"+" "+sub);
+}
+
+function multi(){
+    var num1 = 6;
+    var num2 = 5;
+    var mul = num1*num2;
+    document.write("mul is"+" "+mul);
+}
+
+function division(){
+    var num1 = 6;
+    var num2 = 5;
+    var divs = num1/num2;
+    document.write("divs is"+" "+divs);
+}
+
+
+addition(); //function calling/invoke
+document.write("<br>");
+subtract();
+document.write("<br>");
+multi();
+document.write("<br>");
+division();
